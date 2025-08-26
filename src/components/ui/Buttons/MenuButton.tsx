4c03a46e0ff1d3ecf94cb3706f0dfb23db140563
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-08-26 17:45:42 +0530

    pnmpm issue

--- src/components/ui/Buttons/MenuButton.tsx
+++ src/components/ui/Buttons/MenuButton.tsx
@@ -1,12 +1,20 @@
 import { ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 
+
+
+
 export function MenuButton({
     className, // pulls out className separately
     ...props
+
+
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button
+
+
+        
   className={clsx(
     "w-full bg-blue-600 py-4 text-white text-3xl cursor-pointer hover:bg-blue-700 transition-colors duration-200",
     className
