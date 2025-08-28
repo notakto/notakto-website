a911ac76a812a420d5daeae7c3c43f2a3809dfd3
AshmitSherigar <sherigarashmit@gmail.com>
2025-08-28 08:38:44 +0530

    Refactor : Tailwind classes to match the current styling

--- src/components/ui/Containers/Menu/MenuButtonContainer.tsx
+++ src/components/ui/Containers/Menu/MenuButtonContainer.tsx
@@ -6,7 +6,7 @@ interface MenuButtonContainerProps {
 
 export default function MenuButtonContainer({ children }: MenuButtonContainerProps) {
     return (
-            <div className="flex flex-col items-center gap-4 w-full max-w-md px-6">
+            <div className="flex flex-col items-center gap-4 w-full max-w-md px-8 relative">
                 {children}
             </div>
 
