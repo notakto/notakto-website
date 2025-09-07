241795a9546faf02bc7e63667879ebe1bd1c1ddf
ChehakTrehan <chehak.trehan@gmail.com>
2025-09-07 21:56:37 +0530

    Updated the changes

--- src/modals/TutorialModal.tsx
+++ src/modals/TutorialModal.tsx
@@ -14,7 +14,7 @@ const TutorialModal = () => {
     tabIndex={-1}>
       <header>
         <h1 className="text-3xl text-red-600 text-center mb-4">
-          How&nbsp;&nbsp;&nbsp;to&nbsp;&nbsp;&nbsp;Play Notakto
+          Howto Play Notakto
         </h1>
       </header>
       <section id="tutorial-points" className="text-white text-lg leading-6 mb-6">
