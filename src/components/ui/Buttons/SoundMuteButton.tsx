a5d9340cefbad2d4f15bddf7be76f09bd0c89846
AshmitSherigar <sherigarashmit@gmail.com>
2025-09-10 22:59:40 +0530

    fix typo of text-lg

--- src/components/ui/Buttons/SoundMuteButton.tsx
+++ src/components/ui/Buttons/SoundMuteButton.tsx
@@ -5,7 +5,7 @@ export function SoundMuteButton({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button
-            className={"bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 text-l"}
+            className={"bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 text-lg"}
             {...props}
         />
     );
