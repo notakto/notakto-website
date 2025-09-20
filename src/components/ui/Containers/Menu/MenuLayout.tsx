0633dcbaa4a69d0790763c4f743449dd794f5e48
Snehal Arde <snehalarde02@gmail.com>
2025-09-21 00:34:27 +0530

    fixed image cutting issue

--- src/components/ui/Containers/Menu/MenuLayout.tsx
+++ src/components/ui/Containers/Menu/MenuLayout.tsx
@@ -1,5 +1,5 @@
 export const MenuLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex-1 min-h-screen bg-[url('/background.png')] bg-no-repeat bg-cover flex items-center justify-center">
+  <div className="flex-1 min-h-screen bg-[url('/background.png')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
     {children}
   </div>
 );
