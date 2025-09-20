987db329423584892147b7fd2fddd863f554d812
Snehal Arde <snehalarde02@gmail.com>
2025-09-21 01:36:29 +0530

    reverted the changes

--- src/components/ui/Containers/Menu/MenuLayout.tsx
+++ src/components/ui/Containers/Menu/MenuLayout.tsx
@@ -1,9 +1,5 @@
 export const MenuLayout = ({ children }: { children: React.ReactNode }) => (
-  <div  style={{
-    backgroundImage:
-      "url('/background.png')",
-  }}
-  className="flex-1 min-h-dvh bg-neutral-950 bg-no-repeat bg-cover bg-center flex items-center justify-center">
+  <div className="flex-1 min-h-dvh bg-[url('/background.png')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
     {children}
   </div>
 );
