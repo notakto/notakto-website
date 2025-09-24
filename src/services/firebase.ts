66ae583b92d6463d61b2f0c2e6c05960bc5e36fc
Rakshit Gupta <rakshitg600@gmail.com>
2025-09-25 00:52:38 +0530

    wip

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -31,14 +31,15 @@ if (
 }
 
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: `${NEXT_PUBLIC_FIREBASE_API_KEY}`,
+  authDomain: `${NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${NEXT_PUBLIC_FIREBASE_APP_ID}`,
+  measurementId: `${NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
