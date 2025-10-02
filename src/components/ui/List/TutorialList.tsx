0d074196cceaaa51f2ed63ff5bda46f6e23487d6
AshmitSherigar <sherigarashmit@gmail.com>
2025-10-02 17:48:46 +0530

    Added bullet point

--- src/components/ui/List/TutorialList.tsx
+++ src/components/ui/List/TutorialList.tsx
@@ -5,7 +5,7 @@ interface TutorialListProps {
 const TutorialList = ({ items }: TutorialListProps) => (
   <ul className="text-white text-lg leading-6 mb-6 whitespace-pre-line">
     {items.map((item, idx) => (
-      <li key={idx}>{item}</li>
+      <li key={idx}>• {item}</li>
     ))}
   </ul>
 );
