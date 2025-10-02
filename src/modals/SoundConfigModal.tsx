8fa218aeb36a4f322ae1b9f0e69f5cee23e74265
ChehakTrehan <chehak.trehan@gmail.com>
2025-10-02 16:30:29 +0530

    Replace traditional tags with semantic tags

---
+++ src/modals/SoundConfigModal.tsx
@@ -0,0 +1,88 @@
+'use client'
+import { SoundMuteButton } from "@/components/ui/Buttons/SoundMuteButton";
+import { SoundConfigButton } from "@/components/ui/Buttons/SoundConfigButton";
+import { useSound } from "@/services/store";
+
+type SoundConfigModalProps = {
+    visible: boolean;
+    onClose: () => void;
+};
+
+export default function SoundConfigModal({ visible, onClose }: SoundConfigModalProps) {
+    const {
+        bgMute, bgVolume, setBgMute, setBgVolume,
+        sfxMute, sfxVolume, setSfxMute, setSfxVolume
+    } = useSound();
+
+    const resetSounds = () => {
+        setBgVolume(0.3);
+        setSfxVolume(0.5);
+        // setBgMute(true); // incase reset sound is supposed to make it mute also
+        // setSfxMute(true);
+    };
+
+    if (!visible) return null;
+    return (
+        <dialog open className="fixed inset-0 bg-black/80 flex justify-center items-center z-50">
+            <section className="bg-black p-6 w-[90%] max-w-xl space-y-6 text-center text-white">
+                <header>
+                    <h2 className="text-red-500 text-[35px]">Sound Configuration</h2>
+                </header>
+
+                {/* Background Music */}
+                <div className="my-4 flex items-center justify-between">
+                    <label className="text-red-500 text-2xl flex-1 text-left">
+                        Background Music
+                    </label>
+                    <input
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={Math.round(bgVolume * 100)}
+                        onChange={(e) => setBgVolume(Number(e.target.value) / 100)}
+                        className="flex-2 mx-2 accent-[#0055ff]"
+                    />
+                    <SoundMuteButton
+                        onClick={() => setBgMute(!bgMute)}
+                    >
+                        {bgMute ? "Unmute" : "Mute"}
+                    </SoundMuteButton>
+                </div>
+
+                {/* Player Move */}
+                <div className="my-4 flex items-center justify-between">
+                    <label className="text-red-500 text-2xl flex-1 text-left">
+                        Player Move Sound
+                    </label>
+                    <input
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={Math.round(sfxVolume * 100)}
+                        onChange={(e) => setSfxVolume(Number(e.target.value) / 100)}
+                        className="flex-2 mx-2 accent-[#0055ff]"
+                    />
+                    <SoundMuteButton
+                        onClick={() => setSfxMute(!sfxMute)}
+                    >
+                        {sfxMute ? "Unmute" : "Mute"}
+                    </SoundMuteButton>
+                </div>
+
+                {/* Controls */}
+                <footer className="mt-6 flex flex-wrap gap-3 justify-center">
+                    <SoundConfigButton
+                        onClick={resetSounds}
+                    >
+                        Reset Sounds
+                    </SoundConfigButton>
+                    <SoundConfigButton
+                        onClick={onClose}
+                    >
+                        Return
+                    </SoundConfigButton>
+                </footer>
+            </section>
+        </dialog>
+    );
+}
