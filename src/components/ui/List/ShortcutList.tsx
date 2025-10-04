ec0a50371983c8fb4a2d53e094f1204e74f98944
AshmitSherigar <sherigarashmit@gmail.com>
2025-10-04 11:54:44 +0530

    Refactor ShortcutModal

---
+++ src/components/ui/List/ShortcutList.tsx
@@ -0,0 +1,21 @@
+interface Shortcut {
+  key: string;
+  action: string;
+}
+
+interface ShortcutListProps {
+  shortcuts: Shortcut[];
+}
+
+const ShortcutList = ({ shortcuts }: ShortcutListProps) => (
+  <div className="grid grid-cols-2 gap-4 text-left">
+    {shortcuts.map((shortcut, idx) => (
+      <div key={idx} className="flex flex-col items-start">
+        <h4 className="font-extrabold text-xl">{shortcut.key}</h4>
+        <p className="text-lg text-gray-300">{shortcut.action}</p>
+      </div>
+    ))}
+  </div>
+);
+
+export default ShortcutList;
