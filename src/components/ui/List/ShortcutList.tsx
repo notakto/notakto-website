089a18ba61218e440ddfbef2ed3341e4f767a894
AshmitSherigar <sherigarashmit@gmail.com>
2025-10-04 14:37:30 +0530

    Added to new key to map

--- src/components/ui/List/ShortcutList.tsx
+++ src/components/ui/List/ShortcutList.tsx
@@ -10,7 +10,7 @@ interface ShortcutListProps {
 const ShortcutList = ({ shortcuts }: ShortcutListProps) => (
   <div className="grid grid-cols-2 gap-4 text-left">
     {shortcuts.map((shortcut, idx) => (
-      <div key={idx} className="flex flex-col items-start">
+      <div key={shortcut.key} className="flex flex-col items-start">
         <h4 className="font-extrabold text-xl">{shortcut.key}</h4>
         <p className="text-lg text-gray-300">{shortcut.action}</p>
       </div>
