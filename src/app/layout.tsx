1d9575b0434df67117a282adba5f249e743ff93f
Rakshit Gupta <rakshitg600@gmail.com>
2025-10-07 03:39:32 +0530

    fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "@app/globals.css";
+import "@/app/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { VT323 } from "next/font/google";
