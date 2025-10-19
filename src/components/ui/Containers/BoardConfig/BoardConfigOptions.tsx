c422acf34d383a800a6a2f06040b79e199e3f0a3
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-10-19 18:13:16 +0530

    code rabbit suggestion

--- src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
+++ src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
@@ -8,6 +8,6 @@ export default function BoardConfigOptions({
 	children,
 }: BoardConfigOptionsProps) {
 	return (
-		<menu className="flex flex-wrap gap-2 justify-center">{children}</menu>
+		<div className="flex flex-wrap gap-2 justify-center">{children}</div>
 	);
 }
