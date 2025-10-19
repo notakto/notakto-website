277577e0e08aa1e675a73a3cb6ff1e85e69db036
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-10-19 18:33:51 +0530

    tried something new

--- src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
+++ src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
@@ -7,5 +7,5 @@ interface BoardConfigOptionsProps {
 export default function BoardConfigOptions({
 	children,
 }: BoardConfigOptionsProps) {
-	return <div className="flex flex-wrap gap-2 justify-center">{children}</div>;
+	return <ul className="flex flex-wrap gap-2 justify-center">{children}</ul>;
 }
