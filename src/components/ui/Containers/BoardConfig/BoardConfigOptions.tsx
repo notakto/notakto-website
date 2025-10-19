8884347472f65bfd2a516d5e103ecd4406383bc2
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-10-19 18:07:59 +0530

    post biome -2

--- src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
+++ src/components/ui/Containers/BoardConfig/BoardConfigOptions.tsx
@@ -8,8 +8,6 @@ export default function BoardConfigOptions({
 	children,
 }: BoardConfigOptionsProps) {
 	return (
-		<menu className="flex flex-wrap gap-2 justify-center">
-			{children}
-		</menu>
+		<menu className="flex flex-wrap gap-2 justify-center">{children}</menu>
 	);
 }
