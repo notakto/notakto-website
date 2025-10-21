eb4dc49c3c04d3b9675a6b2b848457e07867874b
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-10-21 18:55:27 +0530

    code rabbit -1

--- src/components/ui/Containers/Cell/CellValueDisplay.tsx
+++ src/components/ui/Containers/Cell/CellValueDisplay.tsx
@@ -7,8 +7,7 @@ export default function CellValueDisplay({ value }: CellValueDisplayProps) {
 
 	return (
 		<div className="absolute inset-0 flex items-center justify-center">
-			<span
-				className="text-red-600 text-5xl leading-none drop-shadow-[0_0_2px_rgba(255,0,0,0.5)]">
+			<span className="text-red-600 text-5xl leading-none drop-shadow-[0_0_2px_rgba(255,0,0,0.5)]">
 				{value}
 			</span>
 		</div>
