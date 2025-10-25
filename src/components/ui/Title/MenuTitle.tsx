dfc9f68e3cc93c42879430729c802e9ed3ba8c8d
anug <anug@gmail.com>
2025-10-25 11:45:52 +0530

    resolving final indentation

--- src/components/ui/Title/MenuTitle.tsx
+++ src/components/ui/Title/MenuTitle.tsx
@@ -2,8 +2,9 @@ import { AnimatedTitle } from "@/components/ui/Effects/AnimatedTitle";
 
 export const MenuTitle = ({ text }: { text: string }) => (
 	<AnimatedTitle
-			text={text}
-			className="-mb-6 md:-mb-8 lg:-mb-10"
-			textClassName="text-red-600 [font-size:clamp(97px,19vw,180px)]"
+		text={text}
+		className="-mb-6 md:-mb-8 lg:-mb-10"
+		textClassName="text-red-600 [font-size:clamp(97px,19vw,180px)]"
 	/>
 );
+
