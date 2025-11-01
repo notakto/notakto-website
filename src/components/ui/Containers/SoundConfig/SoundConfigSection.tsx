f1f0dd4f40464d566c36fad2ac2363cc44dbb01c
anug <anug@gmail.com>
2025-11-01 14:33:44 +0530

    fixing biome tests

--- src/components/ui/Containers/SoundConfig/SoundConfigSection.tsx
+++ src/components/ui/Containers/SoundConfig/SoundConfigSection.tsx
@@ -4,11 +4,7 @@ export default function SoundConfigSection({
 	children,
 }: SoundConfigSectionProps) {
 	return (
-		<div
-			className="
-				my-4 flex items-center justify-between
-				max-[407px]:flex-col max-[407px]:items-start max-[407px]:gap-y-2"
-		>
+		<div className="my-4 flex items-center justify-between max-[407px]:flex-col max-[407px]:items-start max-[407px]:gap-y-2">
 			{children}
 		</div>
 	);
