49cfac13f0780f7a0ac1bb00bba19fa9ff04b8f1
anug <anug@gmail.com>
2025-11-05 13:43:41 +0530

    fixed ui

--- src/components/ui/Title/TutorialTitle.tsx
+++ src/components/ui/Title/TutorialTitle.tsx
@@ -1,4 +1,10 @@
 const TutorialTitle = ({ text }: { text: string }) => (
-	<h2 className="text-4xl text-red-600 text-center mb-4">{text}</h2>
+	<h2
+		className="text-4xl text-red-600 text-center mb-4
+			max-[480px]:text-[1.9rem] max-[400px]:text-[1.7rem]"
+	>
+		{text}
+	</h2>
 );
+
 export default TutorialTitle;
